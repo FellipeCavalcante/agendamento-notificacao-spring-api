566bb6941ae79e9accd791cb3b1b2f9240a6a153
FellipeCavalcante <azevedoaranha1@gmail.com>
2025-02-14 18:28:23 -0300

    Adicionados metodos de cancelamento

--- src/main/java/com/dev/fellipe/agendamento_notificacao_api/controller/AgendamentoController.java
+++ src/main/java/com/dev/fellipe/agendamento_notificacao_api/controller/AgendamentoController.java
@@ -3,6 +3,7 @@ package com.dev.fellipe.agendamento_notificacao_api.controller;
 import com.dev.fellipe.agendamento_notificacao_api.business.AgendamentoService;
 import com.dev.fellipe.agendamento_notificacao_api.controller.dto.in.AgendamentoRecord;
 import com.dev.fellipe.agendamento_notificacao_api.controller.dto.out.AgendamentoRecordOut;
+import com.dev.fellipe.agendamento_notificacao_api.infra.entities.Agendamento;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -28,5 +29,13 @@ public class AgendamentoController {
         var agendamento = agendamentoService.buscarAgendamentoPorId(id);
 
         return ResponseEntity.ok(agendamento);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> cancelarAgendamento(@PathVariable("id") Long id) {
+
+        agendamentoService.cancelarAgendamento(id);
+
+        return ResponseEntity.accepted().build();
     }
 }
