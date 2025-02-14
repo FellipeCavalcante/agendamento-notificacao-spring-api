ea0f9f3a1e74a0bf51df83b319dc2a4c29590d31
FellipeCavalcante <azevedoaranha1@gmail.com>
2025-02-14 17:42:39 -0300

    Adicionados metodos de agendamento de notificacoes

---
+++ src/main/java/com/dev/fellipe/agendamento_notificacao_api/controller/dto/out/AgendamentoRecordOut.java
@@ -0,0 +1,15 @@
+package com.dev.fellipe.agendamento_notificacao_api.controller.dto.out;
+
+import com.dev.fellipe.agendamento_notificacao_api.infra.enums.StatusNotificacaoEnum;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDateTime;
+
+public record AgendamentoRecordOut(Long id,
+                                   String emailDestinatario,
+                                   String telefoneDestinatario,
+                                   String mensagem,
+                                   @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+                                   LocalDateTime dataHoraEnvio,
+                                   StatusNotificacaoEnum statusNotificacao) {
+}
